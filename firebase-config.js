12477117ed9d60be08040c6b15468321ce888513
7az3ma7m3dm-oss <7az3ma7m3dm@gmail.com>
2026-09-26 10:33:20 +0300

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -6,12 +6,11 @@ window.NEXIFING_FIREBASE_CONFIG = {
   authDomain: "nexifing-c3562.firebaseapp.com",
   projectId: "nexifing-c3562",
   storageBucket: "nexifing-c3562.firebasestorage.app",
-  messagingSenderId: "367996673120",
-  appId: "1:367996673120:web:37e1397c4fd04d08a605c5",
-  measurementId: "G-9V3ZLZMFJ2"
+  messagingSenderId: "367996073120",
+  appId: "1:367996073120:web:37e1397c4fd04d08a605c5",
+  measurementId: "G-9V3LTZMFJ2"
 };
 
-// Initialize Firebase (compat SDK)
 if (window.firebase) {
   if (!firebase.apps.length) {
     firebase.initializeApp(window.NEXIFING_FIREBASE_CONFIG);
